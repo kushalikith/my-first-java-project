eac00b1a94df57d5e7082c534925b93d95fc08af
kushal D <kushalikith2001@gmail.com>
2025-08-06 13:31:42 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,40 @@
+public class datatypes {
+    public static void main(String[] args) {
+        // Byte data type
+        byte examScore = 95;
+        System.out.println("Exam Score: " + examScore);
+
+        // Short data type
+        short yearlyFees = 12000;
+        System.out.println("Yearly Fees: " + yearlyFees);
+
+        // Integer data type
+        int age = 25;
+        System.out.println("Age: " + age);
+
+        // Long data type
+        long phoneNumber = 9876543210L;
+        System.out.println("Phone Number: " + phoneNumber);
+
+        // Float data type
+        float height = 5.9f;
+        System.out.println("Height: " + height);
+
+        // Double data type
+        double salary = 50000.50;
+        System.out.println("Salary: " + salary);
+
+        // Character data type
+        char grade = 'A';
+        System.out.println("Grade: " + grade);
+
+        // Boolean data type
+        boolean isEmployed = true;
+        System.out.println("Is Employed: " + isEmployed);
+
+        // String data type
+        String name = "Kushal";
+        System.out.println("Name: " + name);
+
+    }
+}
